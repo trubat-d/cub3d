16643d85930191731aae5f72c840c5a52ae1d7b1
Jeremy Dos santos <jerdos-s@student.42lausanne.ch>
2022-12-22 06:39:16 +0100

    add get_map

--- src_parser/get_map.c
+++ src_parser/get_map.c
@@ -1,14 +1,27 @@
 #include "includes.h"
 
 //TODO: GET MAP TO CHAR ** NOT EXECTUE PARSING INI THE FUNCTION
+static int	ft_strjoin_free(char **final, char *res)
+{
+	char	*tmp;
+
+	if (ft_strjoin(&tmp, *final, res))
+		return (1);
+	del_malloc(*final);
+	del_malloc(res);
+	*final = tmp;
+}
+
 int	get_map(int fd, char ***map, char **buff)
 {
+	char	*final;
 	char	*res;
 
 	(void)map;
+	final = NULL;
 	while (!ft_readline(fd, buff, &res))
-	{
-		del_malloc(res);
-	}
+		ft_strjoin_free(&final, res);
+	if (*buff)
+		ft_strjoin_free(&final, res);
 	return (0);
 }
