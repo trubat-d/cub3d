bd60042d0db78d318b0bf3108799e2e1d6e2b429
Jeremy Dos santos <jerdos-s@student.42lausanne.ch>
2023-01-17 23:24:02 +0100

    finis norme

--- src_parser/fill_map.c
+++ src_parser/fill_map.c
@@ -1,9 +1,32 @@
 #include "includes.h"
 
+static void	condition_fill(int *tab, char c, t_pos pos, int *fill_x)
+{
+	int		tmp;
+	t_data	*data;
+
+	data = get_data(NULL);
+	if (c == '1' || c == '0')
+		tab[(*fill_x)++] = c - '0';
+	else if (c == ' ')
+		tab[(*fill_x)++] = -1;
+	else if (c == '\t')
+	{
+		tmp = 0;
+		while (tmp++ < 4)
+			tab[(*fill_x)++] = -1;
+	}
+	else if (c == 'N')
+	{
+		tab[(*fill_x)++] = 0;
+		data->mlx.player_pos.pos_player.x = pos.x * MAP_SIZE + MAP_SIZE / 2;
+		data->mlx.player_pos.pos_player.y = pos.y * MAP_SIZE + MAP_SIZE / 2;
+	}
+}
+
 void	fill_map(int *tab, char *current_map, int y)
 {
 	t_data	*data;
-	int		tmp;
 	int		str_x;
 	int		fill_x;
 
@@ -12,22 +35,7 @@ void	fill_map(int *tab, char *current_map, int y)
 	data = get_data(NULL);
 	while (current_map[str_x])
 	{
-		if (current_map[str_x] == '1' || current_map[str_x] == '0')
-			tab[fill_x++] = current_map[str_x] - '0';
-		else if (current_map[str_x] == ' ')
-			tab[fill_x++] = -1;
-		else if (current_map[str_x] == '\t')
-		{
-			tmp = 0;
-			while (tmp++ < 4)
-				tab[fill_x++] = -1;
-		}
-		else if (current_map[str_x] == 'N')
-		{
-			tab[fill_x++] = 0;
-			data->mlx.player_pos.pos_player.x = str_x * MAP_SIZE + MAP_SIZE / 2;
-			data->mlx.player_pos.pos_player.y = y * MAP_SIZE + MAP_SIZE / 2;
-		}
+		condition_fill(tab, current_map[str_x], (t_pos){str_x, y}, &fill_x);
 		str_x++;
 	}
 	while (fill_x < data->mlx.map.col)
